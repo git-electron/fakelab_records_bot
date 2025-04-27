2b1d3ec9484416619f14287ddfd731d24776b8fe
raimee <electron.devf@gmail.com>
2025-04-28 02:05:01 +0300

    fix: timezone

--- lib/core/converters/date_time_converter.dart
+++ lib/core/converters/date_time_converter.dart
@@ -4,7 +4,10 @@ class DateTimeConverter implements JsonConverter<DateTime, String> {
   const DateTimeConverter();
 
   @override
-  DateTime fromJson(String json) => DateTime.parse(json).toLocal();
+  DateTime fromJson(String json) {
+    final DateTime data = DateTime.parse(json).toLocal();
+    return data.add(-data.timeZoneOffset);
+  }
 
   @override
   String toJson(DateTime data) => data.toUtc().toIso8601String();
