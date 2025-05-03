e5caa499730b0c81137739e8c5d5602982e7664a
raimee <electron.devf@gmail.com>
2025-05-03 23:58:15 +0300

    Reapply "feat: added dateChanged parameter"
    This reverts commit b6602dc0909845df1aaecad80c7e692afb94b2e8.

--- lib/feature/on_callback/feature/on_callback_confirm/domain/service/create_order_service_impl.dart
+++ lib/feature/on_callback/feature/on_callback_confirm/domain/service/create_order_service_impl.dart
@@ -63,6 +63,7 @@ class CreateOrderServiceImpl implements CreateOrderService {
           ),
         ],
         dateCreated: now,
+        dateChanged: now,
         totalCost: orderType.totalCost,
         costFrom: orderType.costFrom,
         services: _services(orderType),
